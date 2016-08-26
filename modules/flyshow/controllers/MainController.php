92ae78378a84dba59c3e64a9cfd0285cff9c02af
Dirc Wang <dirk.wang@samesamechina.com>
2016-08-26 11:58:25 +0800

    add pgage code

--- modules/flyshow/controllers/MainController.php
+++ modules/flyshow/controllers/MainController.php
@@ -8,7 +8,36 @@ use yii\web\Controller;
 class MainController extends Controller
 {
     public function actionIndex(){
-      return $this->render('index');
+      $data = array(
+        array(
+          'title' => 'title-1',
+          'img' => '/vstyle/img/homepage.jpg',
+          'comment' => '图片来源：Femina',
+        ),
+        array(
+          'title' => 'title-2',
+          'img' => '/vstyle/img/loadpage-2.jpg',
+          'comment' => '图片来源：Femina',
+          'son' => array(
+            array('title' => 'Max Mara群星璀璨', 'img' => '/vstyle/img/pro-1.jpg', 'comment' => '图片来源：Femina'),
+            array('title' => 'Max Mara群星璀璨', 'img' => '/vstyle/img/pro-1.jpg', 'comment' => '图片来源：Femina'),
+            array('title' => 'Max Mara群星璀璨', 'img' => '/vstyle/img/pro-1.jpg', 'comment' => '图片来源：Femina'),
+            array('title' => 'Max Mara群星璀璨', 'img' => '/vstyle/img/pro-1.jpg', 'comment' => '图片来源：Femina'),
+          ),
+        ),
+        array(
+          'title' => 'title-3',
+          'img' => '/vstyle/img/loadpage-3.jpg',
+          'comment' => '图片来源：Femina',
+          'son' => array(
+            array('title' => 'Max Mara群星璀璨', 'img' => '/vstyle/img/pro-1.jpg', 'comment' => '图片来源：Femina'),
+            array('title' => 'Max Mara群星璀璨', 'img' => '/vstyle/img/pro-1.jpg', 'comment' => '图片来源：Femina'),
+            array('title' => 'Max Mara群星璀璨', 'img' => '/vstyle/img/pro-1.jpg', 'comment' => '图片来源：Femina'),
+            array('title' => 'Max Mara群星璀璨', 'img' => '/vstyle/img/pro-1.jpg', 'comment' => '图片来源：Femina'),
+          ),
+        ),
+      );
+      return $this->render('index', array('data' => $data));
     }
 
     public function actionUploadimg(){
--- modules/flyshow/views/main/index.php
+++ modules/flyshow/views/main/index.php
@@ -1,1 +1,4 @@
-daskdkhadjhaskdasd
+<script>
+ var pagecode = <?php print json_encode($data, JSON_UNESCAPED_UNICODE); ?>;
+ console.log(pagecode);
+</script>
