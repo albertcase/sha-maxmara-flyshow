a06609b25afef5dddd6e750589207e5d6a64008d
Dirk Wang <dirk.wang@samesamechina.com>
2016-09-07 00:15:31 +0800

    update image chang

---
+++ web/cimg.php
@@ -0,0 +1,48 @@
+<?php
+require '../models/MyZebraImage.php';
+
+// url = style=w_$width,h_$height&image=$path
+$path = $_GET['image']?$_GET['image']:'';
+$style = $_GET['style']?$_GET['style']:'';
+if(strpos(realpath($path),dirname(__FILE__)) === false){//file not in the up folder
+  Header ('Content-type:image/jpg');
+  print file_get_contents("source/img/unknowimage.jpg");
+  exit;
+}
+
+if(!file_exists($path)){//not exist
+  Header ('Content-type:image/jpg');
+  print file_get_contents("source/img/unknowimage.jpg");
+  exit;
+}
+
+$ar = explode(",", $style);
+$out = array();
+foreach($ar as $x){
+  $x_do = explode("_", $x);
+  if(count($x_do) == 2)
+    $out[$x_do['0']] = $x_do['1'];
+}
+
+$outpath = dirname(realpath($path))."/";
+$opath = $outpath;
+if(isset($out['w']))
+  $outpath = $outpath.'w'.$out['w'];
+if(isset($out['h']))
+  $outpath = $outpath.'h'.$out['h'];
+
+$image = new MyZebraImage($path);
+Header ('Content-type:'.$image->mimeType($image->checkImagetype()));
+if($outpath == $opath || !file_exists($outpath)){
+  print file_get_contents($path);
+  exit;
+}
+
+$outpath = $outpath.'/'.$image->getImagename();
+if(file_exists($outpath)){
+  print file_get_contents($outpath);
+  exit;
+}
+
+$image->resizeOnall($outpath, isset($out['w'])?$out['w']:0, isset($out['h'])?$out['h']:0);
+print file_get_contents($outpath);
